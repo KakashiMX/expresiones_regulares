26f90a1b7612049d6429d19c27fc4b38e8e90128
Ricardo Nava Lima <navalimaricardo@gmail.com>
2020-06-11 10:37:13 -0500

    Validacion de campos

---
+++ campos.js
@@ -0,0 +1,19 @@
+/*  Validación de campos */
+
+
+// Sólo acepta números
+const expresion = /([0-9])+/
+
+
+// Sólo acepta letras, tanto minúsculas como mayúsculas
+const expresion2 = /([a-zA-Z])+/
+
+
+// Sólo acepta letras, tanto minúsculas como mayúsculas, incluyendo la ñ Ñ
+const expresion3 = /([[a-zA-ZñÑ])+/
+
+
+// Sólo acepta letras y números
+const expresion4 = /([[a-zA-Z0-9])+/
+
+
